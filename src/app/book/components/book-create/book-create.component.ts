4c39f0c36f08f80737f06bc9efac0f43fe7597a0
Susana Duque <su.duquem@gmail.com>
2020-09-30 23:36:16 -0500

    Se implementa ngmodel para obtener los valores de los inputs en el componente create

--- src/app/book/components/book-create/book-create.component.ts
+++ src/app/book/components/book-create/book-create.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BookManagerServiceService } from '../../services/book-manager-service.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
+// import { FormControl, FormGroup } from '@angular/forms';
+import { IBooksList } from '../../models/books';
 
 @Component({
   selector: 'app-book-create',
@@ -9,40 +10,37 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./book-create.component.scss'],
 })
 export class BookCreateComponent implements OnInit {
-  newBook: FormGroup;
+  name: string;
+  author: string;
+  imageUrl: string;
+  description: string;
 
   constructor(
     private bookManagerServiceService: BookManagerServiceService,
     private router: Router
   ) {}
 
-  ngOnInit(): void {
-    this.newBook = new FormGroup({
-      name: new FormControl(''),
-      author: new FormControl(''),
-      imageUrl: new FormControl(''),
-      description: new FormControl(''),
-    });
-  }
+  ngOnInit(): void {}
 
-  addNewBook(): void {
-    this.bookManagerServiceService.addNewBook(
-      //   {
-      //   name: 'Brújula3',
-      //   author: 'Diana Uribe',
-      //   imageUrl: 'https://i1.wp.com/nycchildrenstheater.org/wp-content/uploads/2018/06/Harry-Potter_Website.jpg',
-      //   description: 'Brújula3 description',
-      // }
-      {
-        name: this.newBook.value.name,
-        author: this.newBook.value.author,
-        imageUrl: this.newBook.value.imageUrl,
-        description: this.newBook.value.description,
-      }
-    );
-
-    console.log('nuevo libro', this.newBook.value);
-
+  onSubmit(): void {
+    const newBook: IBooksList = {
+      name: this.name,
+      author: this.author,
+      imageUrl: this.imageUrl,
+      description: this.description,
+    };
+    this.bookManagerServiceService.addNewBook(newBook);
     this.router.navigate(['/']);
   }
+
+  // addNewBook(): void {
+  //   this.bookManagerServiceService.addNewBook({
+  //     name: 'Brújula3',
+  //     author: 'Diana Uribe',
+  //     imageUrl:
+  //       'https://i1.wp.com/nycchildrenstheater.org/wp-content/uploads/2018/06/Harry-Potter_Website.jpg',
+  //     description: 'Brújula3 description',
+  //   });
+  //   this.router.navigate(['/']);
+  // }
 }
